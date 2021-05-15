c92ae449e96758e54090010a946f16b422b4b60f
krishnadontaraju <reachkrishnadontaraju@gmail.com>
2021-05-15 12:22:49 +0530

    ADDED : pattern checking ability

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,39 +1,39 @@
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
+
 
 @WebServlet(
 
-    description = "Login to Nirvana",
-    urlPatterns = {"/LoginServlet"},
-    initParams = {
-            @WebInitParam(name = "user" , value = "Sai"),
-            @WebInitParam(name = "password" , value = "testPass")
-        }
+        description = "Login to Nirvana",
+        urlPatterns = {"/LoginServlet"}
 )
 public class LoginServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        //Calling the input given by the user
         String user = request.getParameter("user");
         String pwd = request.getParameter("pwd");
 
-        String userId = getServletConfig().getInitParameter("user");
-        String password = getServletConfig().getInitParameter("password");
-        if (userId.equals(user) && password.equals(pwd)){
-            request.setAttribute("user",user);
-            request.getRequestDispatcher("LoginSuccess.jsp").forward(request,response);
-        } else {
+        Pattern userNamePattern = Pattern.compile("^[A-Z][a-z]{3,}");
+
+        //Checking if the userName given is according to the pattern
+        if (user.matches(String.valueOf(userNamePattern))) {
+            request.setAttribute("user", user);
+            //Forwarding to the successful login jsp file
+            request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+        } else {//Rejecting entry if user name is not according to the pattern
             RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
-            out.println("<font color=red>Wrong Username or Password</font>");
-            dispatcher.include(request,response);
+            out.println("<font color=red>Wrong Username Pattern</font>");
+            dispatcher.include(request, response);
         }
     }
 }
